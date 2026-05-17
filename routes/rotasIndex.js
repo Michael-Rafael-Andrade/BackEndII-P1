3c6893c5ddb0aff33473099b9c28c5e04c6310b3
Michael-Rafael-Andrade <michaelrafaeldeandrade@gmail.com>
2026-05-17 15:10:58 -0300

    instalando o bcrypt e configurando
    instalando o bcrypt

--- routes/rotasIndex.js
+++ routes/rotasIndex.js
@@ -2,9 +2,7 @@ var express = require('express');
 var router = express.Router();
 var controllerIndex = require('../controller//controllerIndex');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// Rota para a página principal (Catálogo de Produtos)
+router.get('/', controllerIndex.tela_principal);
 
 module.exports = router;
